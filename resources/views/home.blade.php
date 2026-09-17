6433f2fbc4ec6e1a5e0c832e6b0ebf3aa81970f8
AmarRanaMagar <NCE078BEI001@nce.edu.np>
2026-09-17 22:41:49 +0545

    Refresh dashboard welcome message

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -81,8 +81,8 @@
                     <div class="row align-items-md-stretch mt-4">
                         <div class="col">
                             <div class="p-3 text-white bg-dark rounded-3">
-                                <h3>Welcome to Unifiedtransform!</h3>
-                                <p><i class="bi bi-emoji-heart-eyes"></i> Thanks for your love and support.</p>
+                                <h3>Welcome to {{ config('app.name', 'School Management System') }}!</h3>
+                                <p><i class="bi bi-speedometer2"></i> Your school management dashboard is ready.</p>
                             </div>
                         </div>
                         <div class="col">
